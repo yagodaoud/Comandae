549479b07ec5a116638179e432d1a5c5f74f1301
yagodaoud <yagodaoud@hotmail.com>
2024-11-24 14:14:50 -0300

    :recycle: Chore: optimize imports

--- src/main/java/com/yagodaoud/comandae/controller/ui/component/MenuGenerationModal.java
+++ src/main/java/com/yagodaoud/comandae/controller/ui/component/MenuGenerationModal.java
@@ -4,10 +4,13 @@ import com.yagodaoud.comandae.model.menu.MenuCategory;
 import com.yagodaoud.comandae.model.menu.MenuItem;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
-import javafx.scene.layout.HBox;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
